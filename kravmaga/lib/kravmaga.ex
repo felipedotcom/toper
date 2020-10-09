403be8cafdf6db6f33cbfeebbc5cdde6a1f01ba6
felipedotcom <felipe.rodolfo29@gmail.com>
2020-10-09 16:05:47 -0300

    intial commit

---
+++ kravmaga/lib/kravmaga.ex
@@ -0,0 +1,18 @@
+defmodule Kravmaga do
+  @moduledoc """
+  Documentation for `Kravmaga`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Kravmaga.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ kravmaga/test/kravmaga_test.exs
@@ -0,0 +1,8 @@
+defmodule KravmagaTest do
+  use ExUnit.Case
+  doctest Kravmaga
+
+  test "greets the world" do
+    assert Kravmaga.hello() == :world
+  end
+end
